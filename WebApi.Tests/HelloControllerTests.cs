01e0454a879f2041ed7bcd3ca260c5344bbcbeec
Alberto Jimenez <60886269+ajeminez@users.noreply.github.com>
2025-07-24 15:53:18 +0200

    Primer commit: app mínima ASP.NET Core con tests

---
+++ WebApi.Tests/HelloControllerTests.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers;
+
+namespace WebApi.Tests;
+
+public class HelloControllerTests
+{
+    [Fact]
+    public void Get_Returns_Hola()
+    {
+        var controller = new HelloController();
+
+        var result = controller.Get() as OkObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal("Hola desde la API", result.Value);
+    }
+}
---
+++ WebApi/Controllers/HelloController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok("Hola desde la API");
+}
